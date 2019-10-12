15c8953ff1013d4fefe05c4de5d6b9e785cf6252
Hiren Patel <nex.hirenpatel@gmail.com>
2019-10-11 23:24:42 -0700

    _

--- src/encryption/DES/des.c
+++ src/encryption/DES/des.c
@@ -12,20 +12,16 @@
 
 #include "des.h"
 
-// int g_ip[] = 
-// {
-// 	58,	50,	42,	34,	26,	18,	10,	2,
-// 	60,	52,	44,	36,	28,	20,	12,	4,
-// 	62,	54,	46,	38,	30,	22,	14,	6,
-// 	64,	56,	48,	40,	32,	24,	16,	8,
-// 	57,	49,	41,	33,	25,	17,	9,	1,
-// 	59,	51,	43,	35,	27,	19,	11,	3,
-// 	61,	53,	45,	37,	29,	21,	13,	5,
-// 	63,	55,	47,	39,	31,	23,	15,	7
-// };
+int		g_ip_2[] = {58, 50, 42, 34, 26, 18, 10, 2,
+	60, 52, 44, 36, 28, 20, 12, 4,
+	62, 54, 46, 38, 30, 22, 14, 6,
+	64, 56, 48, 40, 32, 24, 16, 8,
+	57, 49, 41, 33, 25, 17, 9, 1,
+	59, 51, 43, 35, 27, 19, 11, 3,
+	61, 53, 45, 37, 29, 21, 13, 5,
+	63, 55, 47, 39, 31, 23, 15, 7};
 
-int		g_e[] = {
-	32, 1, 2, 3, 4, 5,
+int		g_e[] = {32, 1, 2, 3, 4, 5,
 	4, 5, 6, 7, 8, 9,
 	8, 9, 10, 11, 12, 13,
 	12, 13, 14, 15, 16, 17,
@@ -34,8 +30,7 @@ int		g_e[] = {
 	24, 25, 26, 27, 28, 29,
 	28, 29, 30, 31, 32, 1};
 
-int		g_sbx[32][16] = {
-	{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+int		g_sbx[32][16] = {{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
 	{0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
 	{4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
 	{15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13},
@@ -172,7 +167,7 @@ static unsigned long long	crypt_des(t_ssl *ssl, unsigned long long block_s)
 	unsigned long long tmp;
 
 	tmp = block_s;
-	block_s = permutate_choice_2(block_s);
+	block_s = permutate(block_s, g_ip_2, 64, 64);
 	if (ssl->encrypt)
 		block_s = des_encrypt_handler(block_s / 4294967296, block_s % 4294967296, ssl->des_subkeys, 0);
 	else
@@ -209,11 +204,6 @@ static void					valid_des(t_ssl *ssl, t_input **input, int i, int j)
 
 int						des_handler(t_ssl *ssl, t_input **input)
 {
-	//char				*output;
-	// unsigned long long	block_s;
-	// uint64_t			i;
-	// int					j;
-	
 	if (!ssl->key)
 		check_hex(ssl, get_input("Enter your key: "));
 	if (ssl->des_cbc && !ssl->vector)
@@ -222,19 +212,7 @@ int						des_handler(t_ssl *ssl, t_input **input)
 	ft_printf("contant = %s | length = %d | encrypt = %d | padded = %d",INPUT->content ,INPUT->length, ssl->encrypt, ssl->padded);
 	ft_printf("\n");
 
-	// while (i < INPUT->length || (ssl->encrypt && ssl->padded == 0))
-	// {
 	valid_des(ssl, input, 0, 0);
-	// 	// i++;
-	// }
-	// if (ssl->mode == 0)
-	// 	output = (char *)des_encrypt_handler(
-	// 				(unsigned char *)input[0]->content, input[0]->length);
-	// else
-	// 	output = (char *)des_decrypt_handler(
-	// 			(unsigned char *)input[0]->content, input[0]->length);
-	//ft_printf("output_fd = %d\n", input[0]->output_file);
-	//write(input[0]->output_file, output, ft_strlen(output));
 	if (input[0]->output_file == 0)
 		ft_printf("\n");
 	return (1);
